bf018ec9c20c164f489da6ab4a13748472030a84
Elvir Dzeko <dzekson94@gmail.com>
2018-04-05 12:50:29 +0200

    heightmap

--- Labb1DatorGrafik/Labb1DatorGrafik/System/ISystem.cs
+++ Labb1DatorGrafik/Labb1DatorGrafik/System/ISystem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Labb1DatorGrafik.System
 {
     interface ISystem
     {
+        void Update(GameTime gameTime);
     }
 }
